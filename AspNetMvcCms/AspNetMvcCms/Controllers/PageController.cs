6d1564becc5a87798773a02faae1f9a7283ef826
busrakrks <115918600+busrakrks@users.noreply.github.com>
2023-02-26 00:27:21 +0300

    26.02.2023 controllerlar eklendi

---
+++ AspNetMvcCms/AspNetMvcCms/Controllers/PageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMvcCms.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult Detail(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ AspNetMvcCms/AspNetMvcCms/Controllers/PostController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMvcCms.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Search(string query,int page)
+        {
+            return View();
+        }
+
+        public IActionResult Detail(int id)
+        {
+            return View();
+        }
+    }
+}
