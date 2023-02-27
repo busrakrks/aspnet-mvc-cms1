c0e76056e0e1207fb9c91114260a8b2e6695b482
busrakrks <115918600+busrakrks@users.noreply.github.com>
2023-02-27 19:17:34 +0300

    Layout düzenlendi

--- AspNetMvcCms/AspNetMvcCms/Controllers/AuthController.cs
+++ AspNetMvcCms/AspNetMvcCms/Controllers/AuthController.cs
@@ -18,3 +18,4 @@ namespace AspNetMvcCms.Controllers
         }
     }
 }
+//busra
--- AspNetMvcCms/AspNetMvcCms/Controllers/HomeController.cs
+++ AspNetMvcCms/AspNetMvcCms/Controllers/HomeController.cs
@@ -4,7 +4,19 @@ namespace AspNetMvcCms.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
         {
             return View();
         }
